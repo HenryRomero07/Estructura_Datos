704e8b0d33daefe9b697a5fb3f03f11795f41c60
HenryRomero07 <jeam.romero@unl.edu.ec>
2026-06-12 13:07:40 -0500

    feat: Learning Javascript for noobscis

---
+++ Unidad02/00Javascript/hi-world.js
@@ -0,0 +1,2 @@
+
+console.log("hi, happy world")
---
+++ Unidad02/00Javascript/tipos-datos.js
@@ -0,0 +1,36 @@
+// PRIMITIVOS: 
+
+//1. STRING
+let nombre = 'Henry'; // String
+
+//2. NUMERICOS
+let edad = 25;
+
+//3. BOOLEANOS
+let esMayorDeEdad = true; 
+
+//4. NULL
+let noHayValor = null;
+
+//5. UNDEFINED
+let valorIndefinido; // No se le asigna ningún valor, por lo que es undefined
+
+//6. SYMBOL VALOR UNICO
+let idUnico = Sumbol('unico'); // Cada símbolo es único, incluso si tienen la misma descripción
+
+//7. datos gigantes bigint
+let numeroGrande = 9007199254740991n; // BigInt para números enteros grandes
+
+//8. OBJETOS
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}; 
+
+//9. ARRAYS 
+let frutas = ['manzana', 'pera', 'piña']; // Array
+
+//10. FUCTIONS
+function saludar(){}
+
+
