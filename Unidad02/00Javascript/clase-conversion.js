53ea7af5b244d931d5e85a1c9ef45fe9a911c5a3
HenryRomero07 <jeam.romero@unl.edu.ec>
2026-06-16 11:36:26 -0500

    feat: Clase del 16 de Junio del 2026, Nombre: Jeam Henry Romero Aguilar

---
+++ Unidad02/00Javascript/clase-conversion.js
@@ -0,0 +1,24 @@
+const string = "47"
+const integer = parseInt(string)
+console.log(integer)
+console.log(typeof integer)
+
+const stringDecimal = "3.14"
+const decimal = parseFloat(stringDecimal)
+console.log(decimal)
+console.log(typeof decimal)
+
+const binary = '10010' 
+const decimal2 = parseInt(binary, 2)
+console.log(decimal2)
+
+const suma = '5' + 3
+console.log(suma)
+console.log(typeof suma)
+
+const suma2 = 5 + true
+console.log(suma2)
+
+
+const suma3 = 5 - true
+console.log(suma3)
